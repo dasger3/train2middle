b46e621d16acde29e9bc9917acba86043f4598b1
Oleksii_Voronin2 <oleksii_voronin2@epam.com>
2022-01-25 15:51:27 +0200

    Add test for wrong input

--- src/main/java/com/epam/ld/module2/testing/template/TemplateEngine.java
+++ src/main/java/com/epam/ld/module2/testing/template/TemplateEngine.java
@@ -3,6 +3,8 @@ package com.epam.ld.module2.testing.template;
 import com.epam.ld.module2.testing.Client;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * The type Template engine.
@@ -24,9 +26,17 @@ public class TemplateEngine {
 
         String[] inputLines = input.split("\n");
 
-        template.setSubject(inputLines[0].substring(inputLines[0].indexOf(" ") + 1));
-        template.setText(inputLines[1].substring(inputLines[1].indexOf(" ") + 1));
-        template.setSender(inputLines[2].substring(inputLines[2].indexOf(" ") + 1));
+        List<String> filteredLines = Arrays.stream(inputLines)
+                .filter(row -> row.startsWith("Subject:") ||
+                        row.startsWith("Text:") ||
+                        row.startsWith("Sender:"))
+                .collect(Collectors.toList());
+
+        if (filteredLines.size() < 3) throw new IllegalArgumentException("Wrong input");
+
+        template.setSubject(filteredLines.get(0).substring(filteredLines.get(0).indexOf(" ") + 1));
+        template.setText(filteredLines.get(1).substring(filteredLines.get(1).indexOf(" ") + 1));
+        template.setSender(filteredLines.get(2).substring(filteredLines.get(2).indexOf(" ") + 1));
 
         return template;
     }
--- src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,8 +1,10 @@
 package com.epam.ld.module2.testing.template;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TemplateEngineTest {
 
@@ -16,5 +18,26 @@ public class TemplateEngineTest {
         Template expected = new Template("Test subject", "Test message text", "voronin@dlit.dp.ua");
 
         assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "Subject: Test subject1\nSender: voronin@dlit.dp.ua",
+            "Subject: Test subject2\n",
+            "Sender: voronin@dlit.dp.ua",
+            "Subject: Test subject\nWrongText: Test message text\nSender: voronin@dlit.dp.ua"
+    })
+    public void checkCreatingTemplateFromWrongInput (String input) {
+
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Template result = templateEngine.createTemplate(input);
+        });
+
+        String expectedMessage = "Wrong input";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
     }
 }
